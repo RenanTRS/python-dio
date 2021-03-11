824402ab160553afa2cf79696f4806edb9445570
RenanTRS <renantrsouza@gmail.com>
2021-03-11 16:39:03 -0400

    Média do aluno com validação de nota

--- aula03.py
+++ aula03.py
@@ -10,7 +10,7 @@ elif a == b == c:
     print('Números iguais')
 elif c > a and c > b:
     print('{} é o maior valor'.format(c))
-'''
+
 n = int(input('Digite um número: '))
 n2 = int(input('Digite um segundo número: '))
 
@@ -21,3 +21,19 @@ if resn == 0 or resn2 == 0:
     print('Um número par foi digitado.')
 else:
     print('Nenhum número par foi digitado.')
+'''
+a = float(input('Nota do primeiro bimestre: '))
+b = float(input('Nota do segundo bimestre: '))
+c = float(input('Nota do terceiro bimestre: '))
+d = float(input('Nota do quarto bimestre: '))
+
+if a <= 10 and b <= 10 and c <= 10 and d <= 10:
+    media = (a + b + c + d)/4
+    if media <= 6:
+        print('Média: {} Reprovado!'.format(media))
+    else:
+        print('Média: {} Aprovado!'.format(media))
+else:
+    print('Alguma nota foi digitada incorretamente.')
+
+#END
