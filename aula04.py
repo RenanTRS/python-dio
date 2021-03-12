5de96b992cd53043aebca0a1bf7fcd27b8f8cc08
RenanTRS <renantrsouza@gmail.com>
2021-03-12 13:46:26 -0400

    for dentro de for

--- aula04.py
+++ aula04.py
@@ -1,4 +1,5 @@
 #Números primos.
+'''
 n = int(input('Digite um número: '))
 cont = 0
 
@@ -11,3 +12,16 @@ if cont == 2:
     print('O número {} é primo.'.format(n))
 else:
     print('O número {} não é primo.'.format(n))
+'''
+n = int(input('Digite um valor: '))
+for num in range(n+1):
+    div = 0
+    for x in range(1, num+1):
+        resto = num % x
+        if resto == 0:
+            div += 1
+
+    if div == 2:
+        print(num)
+
+#END
