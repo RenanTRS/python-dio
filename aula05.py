75d34d27d09a6028cf04a8a087f4db9754db51f3
RenanTRS <renantrsouza@gmail.com>
2021-03-15 14:26:15 -0400

    aula05

---
+++ aula05.py
@@ -0,0 +1,47 @@
+lista = [1, 12, 3, 15, 5, 7]
+lista_animal = ['cachorro', 'gato', 'elefante', 'arara']
+
+'''
+for x in lista_animal:
+    print(x)
+
+soma = 0
+for y in lista:
+    soma += y
+
+print(soma)
+'''
+
+#print(sum(lista)) #soma os valores da lista
+#print(max(lista)) #Mostra o maior valor da lista
+#print(min(lista)) #Mostra o menor valor da lista
+'''
+if 'lobo' in lista_animal:
+    print('existe este item na lista')
+else:
+    print('não existe este item na lista. Será incluído')
+    lista_animal.append('lobo') #Insere um item a lista
+    print(lista_animal)
+'''
+
+#lista_animal.remove('elefante') #Remove itens da lista
+#print(lista_animal)
+
+#lista.sort() #ordena em ordem crescente
+#print(lista)
+#lista_animal.sort() #Ordena em ordem alfabética
+#print(lista_animal)
+
+#lista.reverse() #coloca na ordem inversa
+#print(lista)
+#lista_animal.reverse() #coloca na ordem inversa
+#print(lista_animal)
+
+tupla = (1, 10, 12, 14)
+#print(tupla[0])
+
+#tupla_animal = tuple(lista_animal) #Serve para transformar uma lista em tupla
+#print(tupla_animal)
+
+#lista_numerica = list(tupla) #Serve para transformar uma tupla em lista
+#print(lista_numerica)
